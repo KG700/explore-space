f32becbb1ee9620c496c86749bf4424d72fbc7f3
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-06 13:02:35 +0100

    Set up a reducer and created a store in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer';
 import axios from 'axios';
+
+const store = createStore(reducer);
 
 axios.defaults.baseURL = 'https://api.spacexdata.com/v3'
 
