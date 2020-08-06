6279882290ac07c9ae0a902a93225a3dfd95182b
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-06 13:08:03 +0100

    Wrapped App with Provider in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
+import { Provider } from 'react-redux';
 import axios from 'axios';
 
 const store = createStore(reducer);
@@ -13,9 +14,11 @@ const store = createStore(reducer);
 axios.defaults.baseURL = 'https://api.spacexdata.com/v3'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
