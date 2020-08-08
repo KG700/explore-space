a4a9302ba49d540b7131fbd6f001ec58b9cc699b
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-08 21:01:02 +0100

    Converted FullRocket to a functional component

--- src/components/FullRocket.tsx
+++ src/components/FullRocket.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { IState } from '../store/reducer';
@@ -7,18 +7,15 @@ type Props = {
     rocket: any,
 }
 
-class FullRocket extends Component<Props> {
-
- render () {
-     return (
+const FullRocket = ({ rocket }: Props) => {
+    return (
         <div>
-            <h1>{this.props.rocket.rocket_name}</h1>
-            <p>{this.props.rocket.description}</p>
+            <h1>{rocket.rocket_name}</h1>
+            <p>{rocket.description}</p>
             <h2>Quick Facts</h2>
-     <p>Cost per launch: ${this.props.rocket.cost_per_launch}</p>
+            <p>Cost per launch: ${rocket.cost_per_launch}</p>
         </div>
-     )
- }   
+    )  
 }
 
 const mapStateToProps = (state: IState) => {
