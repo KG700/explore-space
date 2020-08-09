0410ab0c8c26b907089f539f453e3f891bd207f5
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-09 10:47:27 +0100

    Created FullDragon component

---
+++ src/components/FullDragon.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { IState } from '../store/reducer';
+
+type Props = {
+    dragon: any,
+}
+
+const FullDragon = ({ dragon }: Props) => {
+    return (
+        <div>
+            <h1>{dragon.name}</h1>
+            <p>{dragon.description}</p>
+            <h2>Quick Facts:</h2>
+        </div>
+    )  
+}
+
+const mapStateToProps = (state: IState) => {
+    return {
+      dragon: state.selectedDragon
+    };
+  }
+
+export default connect(mapStateToProps)(FullDragon);
