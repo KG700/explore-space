abbe0db1f89ab027b31639036530c725e76e4681
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-09 18:31:59 +0100

    Added statistics to rocket detail modal

--- src/components/SpaceShipDetails/RocketDetails.tsx
+++ src/components/SpaceShipDetails/RocketDetails.tsx
@@ -5,7 +5,7 @@ import { ThunkDispatch } from 'redux-thunk';
 import * as actionCreators from '../../store/actions';
 
 import { IState } from '../../store/reducer';
-import { Modal} from 'antd';
+import { Modal, Statistic, Row, Col } from 'antd';
 
 type Props = {
     rocket: any,
@@ -13,19 +13,54 @@ type Props = {
     onClickNotVisible: () => void
 }
 
-const RocketDetails = ({ rocket, isVisible, onClickNotVisible }: Props) => {
+const RocketDetails = ({ rocket, isVisible, onClickNotVisible }: Props) => {    
     return (
         <div>
-            <Modal
-                title={rocket.rocket_name}
-                visible={isVisible}
-                cancelButtonProps={{ style: { display: 'none' } }}
-                destroyOnClose={true}
-                onOk={onClickNotVisible}
-                onCancel={onClickNotVisible}
-            >
-                <p>{rocket.description}</p>
-            </Modal>
+            { Object.keys(rocket).length > 0 &&
+                <Modal
+                    title={rocket.rocket_name}
+                    visible={isVisible}
+                    cancelButtonProps={{ style: { display: 'none' } }}
+                    destroyOnClose={true}
+                    onOk={onClickNotVisible}
+                    onCancel={onClickNotVisible}
+                >
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Statistic 
+                                title="First Flight" value={rocket.first_flight}
+                            />
+                        </Col>
+                        <Col span={12}>
+                            <Statistic 
+                                title="Country" value={rocket.country}
+                            />
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Statistic 
+                                title="Cost/launch" value={'$' + rocket.cost_per_launch/1000000 + 'm'}
+                            />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic 
+                                title="Height" value={rocket.height.meters.toFixed(0) + 'm'}
+                            />
+                        </Col> 
+                        <Col span={8}>
+                            <Statistic 
+                                title="Mass" value={(rocket.mass.kg/1000).toFixed(0) + "t"}
+                            />
+                        </Col> 
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <p>{rocket.description}</p>
+                        </Col>
+                    </Row>
+                </Modal>
+            }
         </div>
     )  
 }
